d8b8301fe851f901a8d9138491be350d80a2e3ec
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-12 20:11:15 +0700

    connectDb.js

--- connectDb.js
+++ connectDb.js
@@ -3,3 +3,21 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/mean1704')
 .then(() => console.log('Database connected.'))
 .catch(error => console.log('Cannot connect database', error));
+
+const singerSchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true },
+    link: { type: String, required: true, unique: true },
+    image: { type: String }
+});
+
+const Singer = mongoose.model('Singer', singerSchema);
+
+Singer.find({})
+.then(singers => console.log(singers));
+
+// Singer.insertMany([
+//     { name: 'Karik', link: 'https://mp3.zing.vn/nghe-si/Karik', image: 'https://zmp3-photo.zadn.vn/thumb/240_240/avatars/a/0/a0927398989d4c5b18c56880bd56442b_1509531352.jpg' },
+//     { name: 'Đức Phúc', link: 'https://mp3.zing.vn/nghe-si/Duc-Phuc', image: 'https://zmp3-photo.zadn.vn/thumb/240_240/avatars/d/7/d7f34aa6b1112e4b605f6c6e7eccd162_1509437674.jpg' },
+//     { name: 'Châu Khải Phong', link: 'https://mp3.zing.vn/nghe-si/Chau-Khai-Phong', image: 'https://zmp3-photo.zadn.vn/thumb/240_240/avatars/c/a/ca59799621e1c9fd8652cd947713acfa_1509951552.jpg' },
+// ])
+// .then(x => console.log(x));
